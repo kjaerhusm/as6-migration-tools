b04d55d51207f3f71dcc96d3c25cbfd92e5b061d
Michael Weigel <michael.weigel@br-automation.com>
2025-07-31 14:53:34 +0200

    Use splitlines in check_vision_settings
    We already do so everywhere else

--- checks/mapp_vision.py
+++ checks/mapp_vision.py
@@ -8,24 +8,23 @@ def check_vision_settings(apj_path, log, verbose=False):
     log("─" * 80 + "\nChecking mappVision version in project file...")
 
     # Check for mappVision line in the .apj file
-    with apj_path.open(encoding="utf-8", errors="ignore") as f:
-        for line in f:
-            if "<mappVision " in line and "Version=" in line:
-                match = re.search(r'Version="(\d+)\.(\d+)', line)
-                if match:
-                    major = int(match.group(1))
-                    minor = int(match.group(2))
-                    version = f"{major}.{minor}"
+    for line in apj_path.read_text(encoding="utf-8", errors="ignore").splitlines():
+        if "<mappVision " in line and "Version=" in line:
+            match = re.search(r'Version="(\d+)\.(\d+)', line)
+            if match:
+                major = int(match.group(1))
+                minor = int(match.group(2))
+                version = f"{major}.{minor}"
 
-                    log(
-                        f"Found usage of mapp Vision (Version: {version})",
-                        severity="INFO",
-                    )
-                    log(
-                        f"After migrating to AS6 make sure that IP forwarding is activated under the Powerlink interface!",
-                        when="AS6",
-                        severity="MANDATORY",
-                    )
+                log(
+                    f"Found usage of mapp Vision (Version: {version})",
+                    severity="INFO",
+                )
+                log(
+                    f"After migrating to AS6 make sure that IP forwarding is activated under the Powerlink interface!",
+                    when="AS6",
+                    severity="MANDATORY",
+                )
 
     if verbose:
         # Walk through all directories
