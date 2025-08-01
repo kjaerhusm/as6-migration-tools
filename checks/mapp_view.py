37a3a0f1c3b04b2d1a1d9d7732250e848b081417
Tobias Piekut <tobias.piekut@br-automation.com>
2025-08-01 11:04:27 +0200

    Added check for Audit, TextPad, MotionPad or UserList widgets in the mappview script

--- checks/mapp_view.py
+++ checks/mapp_view.py
@@ -1,4 +1,5 @@
 import re
+import xml.etree.ElementTree as ET
 
 
 def check_mappView(apj_path, log, verbose=False):
@@ -29,7 +30,7 @@ def check_mappView(apj_path, log, verbose=False):
                     "\n  MappViewConfiguration->Server Configuration->Startup User: anonymous token"
                     "\n"
                     "\n- Change the following settings in the OPC Client/Server configuration (Configuration View/Connectivity/OpcUaCs/UaCsConfig.uacfg):"
-                    "\n  ClientServerConfiguration->Security->Authentication->Authentication Methods->Anymous: Enabled"
+                    "\n  ClientServerConfiguration->Security->Authentication->Authentication Methods->Anonymous: Enabled"
                     "\n"
                     "\n- Change the following settings in the User role system (Configuration View/AccessAndSecurity/UserRoleSystem/User.user):"
                     '\n  Assign the role "BR_Engineer" to the user "Anonymous". Create that user if it doesn\'t already exist, assign no password.'
@@ -41,6 +42,33 @@ def check_mappView(apj_path, log, verbose=False):
                     when="AS6",
                     severity="WARNING",
                 )
+
+            # check for specific widgets
+            # Namespace mappings
+            ns = {
+                "c": "http://www.br-automation.com/iat2015/contentDefinition/v2",
+                "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+            }
+            logical_path = apj_path.parent / "Logical"
+            for content_path in logical_path.rglob("*.content"):
+                tree = ET.parse(content_path)
+                root_elem = tree.getroot()
+
+                for widget in root_elem.findall(".//c:Widget", ns):
+                    xsi_type = widget.attrib.get(f"{{{ns['xsi']}}}type")
+                    if xsi_type in {
+                        "widgets.brease.AuditList",
+                        "widgets.brease.TextPad",
+                        "widgets.brease.UserList",
+                        "widgets.brease.MotionPad",
+                    }:
+                        log(
+                            "Found use of AuditList, UserList, TextPad or MotionPad widgets that requires the role of BR_Engineer",
+                            severity="INFO",
+                        )
+                        log(
+                            "Check in the following (Configuration View/AccessAndSecurity/UserRoleSystem/User.user) that a user with role BR_Engineer is present"
+                        )
 
     if verbose:
         # Walk through all directories
